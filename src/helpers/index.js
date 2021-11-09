30d53aca4caaeb763ba7c753996921a2c8be718d
Alexander Ivankov <moonw1nd@yandex-team.ru>
2021-11-09 14:41:13 +0300

    refactor: helpers in functional style

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,28 +2,29 @@ import fs from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import chalk from 'chalk';
+import { complement, compose, partial, partialRight, when } from 'ramda';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export const readFile = (filename) => {
-    const filePath = path.resolve(__dirname, '../', filename);
+const pathResolve = partial(path.resolve, [__dirname, '../']);
+const readUtfFile = partialRight(fs.readFileSync, [{ encoding: 'utf8' }]);
 
-    return fs.readFileSync(filePath, { encoding: 'utf8' });
-};
+export const readFile = compose(readUtfFile, pathResolve);
 
 export const writeFile = (filename, filepath, data) => {
-    const filePath = path.join(__dirname, '../', filepath, filename);
+    const filePath = pathResolve(filepath, filename);
 
     fs.writeFileSync(filePath, data);
 };
 
 const printToConsole = console.log;
+const isString = (value) => typeof value === 'string';
+const isNotString = complement(isString);
+const prepareValueForLog = when(isNotString, JSON.stringify);
 
 export const log = (color, title, value) => {
-    if (typeof value !== 'string') {
-        value = JSON.stringify(value);
-    }
+    value = prepareValueForLog(value);
 
     printToConsole(
         `${chalk[color](`[${title.toUpperCase()}] `)}${chalk.white(
