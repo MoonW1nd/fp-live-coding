3aa6055a0798512a274b9dff256ab3e6d64953bd
Andrey <andreyadad@gmail.com>
2024-07-28 16:37:28 +0500

    Подготовился к началу урока, заменил все в главном файле на императивный стиль Заодно подредактировал исходный файл логов чтобы он выдавал три ошибки вместо двух как в примере

--- src/index.js
+++ src/index.js
@@ -1,81 +1,49 @@
-import {
-    allPass,
-    anyPass,
-    applySpec,
-    compose,
-    curry,
-    equals,
-    filter,
-    map,
-    match,
-    nth,
-    partial,
-    partialRight,
-    prop,
-    tap,
-    tryCatch,
-} from 'ramda';
 import { log, readFile, writeFile } from './helpers/index';
 
 console.clear();
 
-const curriedLog = curry(log);
-const logRed = curriedLog('red');
-const logGreen = curriedLog('green');
-const logError = logRed('Error');
-const logReadFile = logGreen('Read file');
-const logWriteFile = logGreen('Write file');
+const path = process.env.FILE_PATH;
 
-const formatLogs = partialRight(JSON.stringify, [null, 2]);
-const writeErrorLogs = partial(writeFile, ['errorsLog.json', '../']);
+log('green', 'Read file', path);
 
-const getMessage = prop('message');
-const getType = prop('type');
-const getComponent = prop('component');
-const getParsedLogTime = nth(1);
-const getParsedLogType = nth(2);
-const getParsedLogComponent = nth(3);
-const getParsedLogMassage = nth(4);
+let fileData;
 
-const logErrorMessage = compose(logError, getMessage);
+try {
+    fileData = readFile(path);
+} catch (e) {
+    log('red', 'Error', e.message);
+}
 
-const isError = equals('ERROR');
-const isWarn = equals('WARN');
-const isInfraComponent = equals('search-interfaces-infra');
-const isErrorLog = compose(isError, getType);
-const isWarnLog = compose(isWarn, getType);
-const isInfraComponentLog = compose(isInfraComponent, getComponent);
+const logs = fileData.match(/[^\r\n]+/g);
 
-const isErrorOrWarnLog = anyPass([isErrorLog, isWarnLog]);
-const isInfraErrorLog = allPass([isErrorOrWarnLog, isInfraComponentLog]);
+const logsWithErrors = [];
 
-const splitFileByLine = match(/[^\r\n]+/g);
-const parseLog = match(/([\d-:,\s]+)\s\(.+\)\s(\w+)\s+\(([^)]+)\)\s(.+)/);
-const getParsedLogInfo = applySpec({
-    time: getParsedLogTime,
-    type: getParsedLogType,
-    component: getParsedLogComponent,
-    message: getParsedLogMassage,
-});
+for (let i = 0; i < logs.length; i++) {
+    const logData = logs[i].match(
+        /([\d-:,\s]+)\s\(.+\)\s(\w+)\s+\(([^)]+)\)\s(.+)/,
+    );
 
-const createSafeFunction = (fn) => tryCatch(fn, logErrorMessage);
-const readFileSafe = createSafeFunction(readFile);
-const writeErrorLogsSafe = createSafeFunction(writeErrorLogs);
+    const logInfo = {
+        time: logData[1],
+        type: logData[2],
+        component: logData[3],
+        message: logData[4],
+    };
 
-const getInfraErrorsLog = compose(
-    filter(isInfraErrorLog),
-    map(getParsedLogInfo),
-    map(parseLog),
-);
+    if (
+        (logInfo.type === 'ERROR' || logInfo.type === 'WARN') &&
+        logInfo.component === 'search-interfaces-infra'
+    ) {
+        logsWithErrors.push(logInfo);
+    }
+}
 
-const app = compose(
-    writeErrorLogsSafe,
-    tap(logWriteFile),
-    formatLogs,
-    getInfraErrorsLog,
-    splitFileByLine,
-    readFileSafe,
-    tap(logReadFile),
-);
+const formatedLogs = JSON.stringify(logsWithErrors, null, 2);
 
-app(process.env.FILE_PATH);
+log('green', 'Write to file', formatedLogs);
+
+try {
+    writeFile('errorsLog.json', '../', formatedLogs);
+} catch (e) {
+    log('red', 'Error', e.message);
+}
